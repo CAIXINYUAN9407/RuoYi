7429172486e68856399bc0152997d4bca09c97ad
CAIXINYUAN9407 <1057796146@qq.com>
2024-07-05 14:06:27 +0800

    第三方登录

---
+++ ruoyi-admin/src/main/resources/static/ruoyi/otherLogin.js
@@ -0,0 +1,26 @@
+
+$(function() {
+    $.ajax({
+        type: "post",
+        url: ctx + "OtherLoginAjax",
+
+        data: {
+            "code": "111"
+        },
+        beforeSend: function () {
+            $.modal.loading($("#btnSubmit").data("loading"));
+        },
+        success: function(r) {
+            if (r.code == web_status.SUCCESS) {
+                location.href = ctx + 'index';
+            } else {
+                $('.imgcode').click();
+                $(".code").val("");
+                $.modal.msg(r.msg);
+            }
+            $.modal.closeLoading();
+        }
+    });
+});
+
+
